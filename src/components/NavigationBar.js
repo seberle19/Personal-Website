2a75c3db4cfeec1468e54a87ceceb38defc27838
Scott Eberle <seberle1@swarthmore.edu>
2020-07-07 16:49:17 -0400

    Initial commit

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import NavBarMenu from './NavBarMenu'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <NavBarMenu/>
+          <Typography variant="h6" className={classes.title}>
+            Scott Eberle
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
