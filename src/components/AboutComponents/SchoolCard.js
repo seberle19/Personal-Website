f95a6d5911fe95fb0d9b94d28bac3c66544b4853
Scott Eberle <seberle1@swarthmore.edu>
2020-07-10 15:03:17 -0400

    Changed font color

--- src/components/AboutComponents/SchoolCard.js
+++ src/components/AboutComponents/SchoolCard.js
@@ -4,11 +4,13 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import SwatPic from './photos/Swarthmore.jpg';
 
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    color: '#d32f2f',
   },
 
   title: {
@@ -21,6 +23,9 @@ const useStyles = makeStyles({
     height: 500,
     paddingTop: 0, // 16:9
   },
+  color: {
+    textPrimary: '#d32f2f',
+  }
 });
 
 export default function SimpleCard() {
@@ -35,15 +40,17 @@ export default function SimpleCard() {
         </Typography>
         <Typography variant="h6" component="h3">
           I am currently getting a B.A. in Computer Science at
-          Swarthmore college. Computer science has become my favority subject,
+          Swarthmore college. Computer science has become my favorite subject,
           but the liberal arts education is what really drew to me Swarthmore.
           I have taken some really awesome courses outside of my major on
           subjects in ancient history and cognitive science.
         </Typography>
       <CardMedia
+        component="img"
+        src={SwatPic}
+        alt="Parrish Hall"
+        height="140"
         className={classes.media}
-        image={require("./photos/Swarthmore.jpg")}
-        title="Contemplative Reptile"
       />
       </CardContent>
     </Card>
